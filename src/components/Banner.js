821e11e5d8aacb7e9e30ee191b205febe233da20
Aissam <aissamrahmani@gmail.com>
2021-11-14 14:23:17 +0100

    add condition to rempotez plant

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,10 +5,23 @@ import '../styles/Banner.css'
 function Banner(){
 
     const title = 'La maison jungule'
+    const currentMonth = new Date().getMonth()
+    const isSpring = currentMonth >= 2  && currentMonth <= 5
+    console.log(currentMonth +1)
+
+    const recommentdation = isSpring ? (
+        <div> C'est le pritemps, rempotez</div>
+    ) :(
+        <div> Ce n'est apas le moment de rempotez !</div>
+    )
+
     return (
      <div className='lmj-banner'>
-        <img src={logo}  alt='La maison jungule' className='lmj-logo'/>
-        <h1 className='lmj-title'> {title} </h1>
+            <div className='lmj-banner-row'> 
+                <img src={logo}  alt='La maison jungule' className='lmj-logo'/>
+                <h1 className='lmj-title'> {title} </h1>
+            </div>
+            {recommentdation}
       </div>
     )
 }
