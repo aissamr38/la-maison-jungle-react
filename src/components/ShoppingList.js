f3e82b9dd25fda58eb1628b6cd87d38f5095150b
Aissam <aissamrahmani@gmail.com>
2021-11-14 14:31:27 +0100

    add css shoppingList

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import {plantList} from '../datas/plantList'
+import '../styles/ShoppingList.css'
 
 function ShoppingList(){
 
@@ -18,9 +19,9 @@ function ShoppingList(){
                     <li key={cat}>{cat}</li>
                 ))}
             </ul>
-            <ul>
+            <ul className='lmj-plant-list'> 
                 {plantList.map((plant) => (
-                    <li key={plant.id}>{plant.name} {plant.isBestSale ? <span>🔥</span> : null} </li>
+                    <li key={plant.id} className='lmj-plant-item'>{plant.name} {plant.isBestSale ? <span>🔥</span> : null} </li>
                 ))}
             </ul>
         </div>
