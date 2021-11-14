44440c8032225d476d1b5819f3200e91f6d0f165
Aissam <aissamrahmani@gmail.com>
2021-11-14 02:09:10 +0100

    add data Base list

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,24 +1,15 @@
-
-
-const plantList = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
-
+import {plantList} from '../datas/plantList'
 
 function ShoppingList(){
 
 
     return (
         <ul>
-            {
+           {/*
                 plantList.map((plant,index) =>(
                     <li key={`${plant}-${index}`}>{plant}</li>
                 ))
-            }
+                */}
         </ul>
     )
 }
